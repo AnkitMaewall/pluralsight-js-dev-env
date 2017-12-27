ef492c2d212bbade6e358e7977d0397318e0a4bd
Ankit Maewall <Ankit.Maewall@gmail.com>
2017-12-27 01:29:22 -0500

    module 11 finished

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -15,7 +15,7 @@ const json = JSON.stringify(jsf(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
   if (err) {
-    return console.log(echalk.red(err));
+    return console.log(chalk.red(err));
   } else {
     console.log(chalk.green("Mock data generated."));
   }
